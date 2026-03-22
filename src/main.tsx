c428cabd61b014a3f3499ba5f7d3525df627d95d
Samuel Nunes <samuel97.sn@gmail.com>
2026-03-22 18:18:16 -0300

    alterações iniciais conforme o andamento da aula

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import QualquerCoisa from "./App";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QualquerCoisa />
+    <div>
+      <h1>Olá mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
+        Incidunt accusamus comodi quasi sit, doloribus in fugiat, repellat
+        Laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
+        quia rerum!
+      </p>
+    </div>
   </StrictMode>,
-)
+);
