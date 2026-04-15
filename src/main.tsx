17e7923460272bf8c9c48910c9c4e498632b1075
Samuel Nunes <samuel97.sn@gmail.com>
2026-04-14 22:50:29 -0300

    subclase compenent heading estava com nome errado e fiz moficação

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import QualquerCoisa from "./App";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QualquerCoisa />
-    <div>
-      <h1>Olá mundo!</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        Incidunt accusamus comodi quasi sit, doloribus in fugiat, repellat
-        Laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
-      </p>
-    </div>
+    <App />
   </StrictMode>,
 );
